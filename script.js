e0d0ece7f516c5ca7e78fa584ccf26d4062be486
diogofontte <diogofontte.design@gmail.com>
2024-06-20 11:58:29 +0100

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  // pegar na tag img
+  const img = document.querySelector("#profile img")
+  // substituir a imagem
+  if (html.classList.contains('light')){
+    //se tiver light mode, adicionar imagem light
+    img.setAttribute('src', './assets/photo-avatar-light.jpg')
+  } else {
+    //se tiver sem light mode, manter imagem normal
+    img.setAttribute('src', './assets/photo-avatar.jpg')
+  }
+}
